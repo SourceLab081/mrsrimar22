mrsrimar22/android_kernel_xiaomi_sm6225

techpack: display: Implement esd recovery
Change-Id: 9a4b59b7bae0d2f47ef66c3e64498ded7f46c90b
Signed-off-by: mrsrimar22 <mrsrimar22@gmail.com>

techpack/display/msm/sde/sde_connector.c
@@ -939,6 +939,27 @@ int sde_connector_clk_ctrl(struct drm_connector *connector, bool enable)
 	return rc;
 }
 
+void sde_connector_esd_irq_cleanup(struct sde_connector *c_conn)
+{
+	int irq;
+
+	if (!c_conn)
+		return;
+
+	if (!c_conn->esd_irq_registered)
+		return;
+
+	irq = c_conn->esd_irq;
+	if (irq > 0) {
+		disable_irq(irq);
+		synchronize_irq(irq);
+		free_irq(irq, c_conn);
+	}
+
+	c_conn->esd_irq_registered = false;
+	c_conn->esd_irq = 0;
+}
+
 void sde_connector_destroy(struct drm_connector *connector)
 {
 	struct sde_connector *c_conn;
@@ -955,6 +976,8 @@ void sde_connector_destroy(struct drm_connector *connector)
 
 	if (c_conn->ops.pre_destroy)
 		c_conn->ops.pre_destroy(connector, c_conn->display);
+
+	sde_connector_esd_irq_cleanup(c_conn);
 
 	if (c_conn->blob_caps)
 		drm_property_blob_put(c_conn->blob_caps);
@@ -2173,6 +2196,51 @@ static void _sde_connector_report_panel_dead(struct sde_connector *conn,
 			conn->base.base.id, conn->encoder->base.id);
 }
 
+static irqreturn_t esd_err_irq_handle(int irq, void *data)
+{
+	struct sde_connector *c_conn = data;
+	struct dsi_display *dsi_display;
+	struct dsi_panel *panel;
+	struct gpio_desc *desc;
+	bool panel_on = false;
+	int err_irq_gpio_value = 1;
+	int ret = 0;
+
+	if (!c_conn || !c_conn->display) {
+		SDE_ERROR("not able to get connector object\n");
+		return IRQ_HANDLED;
+	}
+
+	if (c_conn->connector_type != DRM_MODE_CONNECTOR_DSI)
+		return IRQ_HANDLED;
+
+	dsi_display = (struct dsi_display *)c_conn->display;
+	if (!dsi_display || !dsi_display->panel)
+		return IRQ_HANDLED;
+
+	panel = dsi_display->panel;
+	panel_on = dsi_panel_initialized(panel);
+	if (!panel_on)
+		return IRQ_HANDLED;
+
+	ret = atomic_cmpxchg(&c_conn->esd_pending, 0, 1);
+	if (ret != 0)
+		return IRQ_HANDLED;
+
+	desc = gpio_to_desc(panel->esd_config.esd_err_irq_gpio);
+	if (desc)
+		err_irq_gpio_value = gpiod_get_value(desc);
+	else
+		err_irq_gpio_value = gpio_get_value(panel->esd_config.esd_err_irq_gpio);
+
+	if (panel_on && !c_conn->panel_dead && err_irq_gpio_value == 0)
+		_sde_connector_report_panel_dead(c_conn, false);
+
+	atomic_set(&c_conn->esd_pending, 0);
+
+	return IRQ_HANDLED;
+}
+
 int sde_connector_esd_status(struct drm_connector *conn)
 {
 	struct sde_connector *sde_conn = NULL;
@@ -2471,6 +2539,23 @@ static int _sde_connector_install_properties(struct drm_device *dev,
 				sizeof(dsi_display->panel->hdr_props),
 				CONNECTOR_PROP_HDR_INFO);
 		}
+
+		/* register esd irq and enable it after panel enabled */
+		if (dsi_display && dsi_display->panel &&
+			dsi_display->panel->esd_config.esd_err_irq > 0) {
+			int irq = dsi_display->panel->esd_config.esd_err_irq;
+			unsigned long irqflags = dsi_display->panel->esd_config.esd_err_irq_flags;
+
+			rc = request_threaded_irq(irq, NULL, esd_err_irq_handle,
+					irqflags, "esd_err_irq", c_conn);
+			if (rc < 0) {
+				SDE_ERROR("request irq(%d) failed, rc=%d\n", irq, rc);
+			} else {
+				SDE_INFO("requested esd irq %d (irqflags=0x%lx)\n", irq, irqflags);
+				c_conn->esd_irq = irq;
+				c_conn->esd_irq_registered = true;
+			}
+		}
 	}
 
 	msm_property_install_volatile_range(
@@ -2688,6 +2773,10 @@ struct drm_connector *sde_connector_init(struct drm_device *dev,
 			SDE_CONN_EVENT_VID_FIFO_OVERFLOW,
 			sde_connector_handle_disp_recovery,
 			c_conn);
+
+	atomic_set(&c_conn->esd_pending, 0);
+	c_conn->esd_irq = 0;
+	c_conn->esd_irq_registered = false;
 
 	rc = _sde_connector_install_properties(dev, sde_kms, c_conn,
 		connector_type, display, &display_info);

SourceLab081/mrsrimar22

techpack/display/msm/sde/sde_connector.c
@@ -939,6 +939,27 @@ int sde_connector_clk_ctrl(struct drm_connector *connector, bool enable)
 	return rc;
 }
 
+void sde_connector_esd_irq_cleanup(struct sde_connector *c_conn)
+{
+	int irq;
+
+	if (!c_conn)
+		return;
+
+	if (!c_conn->esd_irq_registered)
+		return;
+
+	irq = c_conn->esd_irq;
+	if (irq > 0) {
+		disable_irq(irq);
+		synchronize_irq(irq);
+		free_irq(irq, c_conn);
+	}
+
+	c_conn->esd_irq_registered = false;
+	c_conn->esd_irq = 0;
+}
+
 void sde_connector_destroy(struct drm_connector *connector)
 {
 	struct sde_connector *c_conn;
@@ -955,6 +976,8 @@ void sde_connector_destroy(struct drm_connector *connector)
 
 	if (c_conn->ops.pre_destroy)
 		c_conn->ops.pre_destroy(connector, c_conn->display);
+
+	sde_connector_esd_irq_cleanup(c_conn);
 
 	if (c_conn->blob_caps)
 		drm_property_blob_put(c_conn->blob_caps);
@@ -2173,6 +2196,54 @@ static void _sde_connector_report_panel_dead(struct sde_connector *conn,
 			conn->base.base.id, conn->encoder->base.id);
 }
 
+static irqreturn_t esd_err_irq_handle(int irq, void *data)
+{
+	struct sde_connector *c_conn = data;
+	struct dsi_display *dsi_display;
+	struct dsi_panel *panel;
+	struct gpio_desc *desc;
+	bool panel_on = false;
+	int gpio_val = -1;
+	int ret = 0;
+
+	if (!c_conn || !c_conn->display) {
+		SDE_ERROR("not able to get connector object\n");
+		return IRQ_HANDLED;
+	}
+
+	if (c_conn->connector_type != DRM_MODE_CONNECTOR_DSI)
+		return IRQ_HANDLED;
+
+	dsi_display = (struct dsi_display *)c_conn->display;
+	if (!dsi_display || !dsi_display->panel)
+		return IRQ_HANDLED;
+
+	panel = dsi_display->panel;
+
+	desc = gpio_to_desc(panel->esd_config.esd_err_irq_gpio);
+	if (desc)
+		gpio_val = gpiod_get_value(desc);
+	else
+		gpio_val = gpio_get_value(panel->esd_config.esd_err_irq_gpio);
+
+	panel_on = dsi_panel_initialized(panel);
+	SDE_INFO("esd irq fired: gpio=%d val=%d panel_on=%d\n",
+		panel->esd_config.esd_err_irq_gpio, gpio_val, panel_on);
+
+	if (!panel_on)
+		return IRQ_HANDLED;
+
+	ret = atomic_cmpxchg(&c_conn->esd_pending, 0, 1);
+	if (ret != 0)
+		return IRQ_HANDLED;
+
+	_sde_connector_report_panel_dead(c_conn, false);
+
+	atomic_set(&c_conn->esd_pending, 0);
+
+	return IRQ_HANDLED;
+}
+
 int sde_connector_esd_status(struct drm_connector *conn)
 {
 	struct sde_connector *sde_conn = NULL;
@@ -2471,6 +2542,23 @@ static int _sde_connector_install_properties(struct drm_device *dev,
 				sizeof(dsi_display->panel->hdr_props),
 				CONNECTOR_PROP_HDR_INFO);
 		}
+
+		/* register esd irq and enable it after panel enabled */
+		if (dsi_display && dsi_display->panel &&
+			dsi_display->panel->esd_config.esd_err_irq > 0) {
+			int irq = dsi_display->panel->esd_config.esd_err_irq;
+			unsigned long irqflags = dsi_display->panel->esd_config.esd_err_irq_flags;
+
+			rc = request_threaded_irq(irq, NULL, esd_err_irq_handle,
+					irqflags, "esd_err_irq", c_conn);
+			if (rc < 0) {
+				SDE_ERROR("request irq(%d) failed, rc=%d\n", irq, rc);
+			} else {
+				SDE_INFO("requested esd irq %d (irqflags=0x%lx)\n", irq, irqflags);
+				c_conn->esd_irq = irq;
+				c_conn->esd_irq_registered = true;
+			}
+		}
 	}
 
 	msm_property_install_volatile_range(
@@ -2688,6 +2776,10 @@ struct drm_connector *sde_connector_init(struct drm_device *dev,
 			SDE_CONN_EVENT_VID_FIFO_OVERFLOW,
 			sde_connector_handle_disp_recovery,
 			c_conn);
+
+	atomic_set(&c_conn->esd_pending, 0);
+	c_conn->esd_irq = 0;
+	c_conn->esd_irq_registered = false;
 
 	rc = _sde_connector_install_properties(dev, sde_kms, c_conn,
 		connector_type, display, &display_info);